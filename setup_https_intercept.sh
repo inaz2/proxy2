e35425438e9fdbeac6f6358886cc5a8fcea2a79a
Vitaliy Zhaborovskyy <zvi@ciklum.com>
2019-10-23 15:52:43 +0300

    setup_https_intercept.sh updated

--- setup_https_intercept.sh
+++ setup_https_intercept.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-openssl genrsa -out ca.key 2048
-openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=proxy2 CA"
-openssl genrsa -out cert.key 2048
-mkdir certs/
+mkdir ssl-data/
+openssl genrsa -out ssl-data/ca.key 2048
+openssl req -new -x509 -days 3650 -key ssl-data/ca.key -out ssl-data/ca.crt -subj "/CN=proxy2 CA"
+openssl genrsa -out ssl-data/cert.key 2048
+mkdir ssl-data/certs/
